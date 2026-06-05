e033865e74910a60e58d99ffc88e3a2306fa4741
Lucian Ciuraru <0ciurg64@solent.ac.uk>
2026-06-05 20:55:43 +0100

    Doing 2 problems daily. Today:Single Number and Majority Element

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+namespace LeetCodeProblems
+{
+    class LeetCodeProblems
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 2, 1 };
+            int count = 0;
+            int result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                count = numbers.Count(n => n == 2);
+                result = result + count;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
